6eb41fb9fbac25e79c632d77598c3db7a5d13bb3
Vy Thuy Nguyen <vng0510@gmail.com>
2017-02-21 01:38:34 -0500

    more changes to graph gen

--- GenGraph.py
+++ GenGraph.py
@@ -10,31 +10,47 @@ def genGraph(argv):
     rnd = snap.TRnd()
     graph = snap.GenPrefAttach(node_count, node_deg, rnd)
     edge_count = len(list(graph.Edges()))
-
+    per_proc = math.floor(edge_count / p) #default to using floor
+    
     #determine if to use ceil or floor
     #math.ceil(<num>); math.floor(<num>)
     if edge_count % p >= p / 2:
-        print 'p = %d, edge_count = %d, p/2 = %f => use ceil\n' % (p, edge_count, p/2)
-    else:  
-        print 'p = %d, edge_count = %d, p/2 = %f =>  use floor\n' % (p, edge_count, p/2)
+        per_proc = math.ceil(edge_count)
         
-    #graph_file_name = 'g' + str(node_count) + '_' + str(node_deg)
-    #graph_file = open(graph_file_name, 'w')
+    graph_file_name = 'g' + str(node_count) + '_' + str(node_deg)
+    graph_file = open(graph_file_name, 'w')
 
     #start with proc 0
     #update every edge_count/p
-    #per_proc_file_name = graph_file_name + '0'
-    #per_proc_file = open(per_proc_file_name)
-            
-    #count = 0
+    per_proc_file_name = graph_file_name + '0'
+    per_proc_file = open(per_proc_file_name, 'w')
+    count = 0
+    cur_proc = 0
+    cur_id = 0
 
-            
-#    for edge in graph.Edges(): Write the edge list to file print "%d
-        # -- %d;" % (edge.GetSrcNId(), edge.GetDstNId())
+    #TODO: CURRENTLY ASSUME p == 4!!!
+    # for p != 4 => DEBUG!!! NOT SURE WHAT'LL HAPPEN!!
+    for edge in graph.Edges():
+        #Write the edge list to file
+        print "%d -- %d;" % (edge.GetSrcNId(), edge.GetDstNId())
 
         # Additionally, write the edge ID to the appropriate file for
         # ea proc ++count
+        if count < per_proc:
+            #write to file
+            per_proc_file.write(str(cur_id) + '\n')
+            cur_id = cur_id + 1
+            count = count + 1
+        else: #update the file
+            cur_proc = cur_proc + 1
 
+            #if this is the last iter => anything special?
+            
+            count = 0 #reset the count
+            per_proc_file_name = graph_file_name + str(cur_proc)
+            per_proc_file.close()
+            per_proc_file = open(per_proc_file_name, 'w')
+            
 
 def writeToFile(data, output_file):
     # write data to output_file
