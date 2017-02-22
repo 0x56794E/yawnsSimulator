6758aedfc7f3e0d11b713e3ec73ecf7f0867ba19
Vy Thuy Nguyen <vng0510@gmail.com>
2017-02-21 23:48:41 -0500

    util for finding LP's neighbors

--- java/util/NeighborFinder.java
+++ java/util/NeighborFinder.java
@@ -5,9 +5,28 @@
  * Input: edge list file
  * ASSUMPTION: link id is the order it appears in the edge list file
  */
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 public class NeighborFinder
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        if (args.length < 1)
+        {
+            System.out.println("Must provide input file name");
+            System.exit(1);                
+        }
+
+        
+        doGenerate(args[0]);
+                
+    }
+
+    private static void doGenerate(String fileName) throws IOException
+    {
+        Files.lines(Paths.get(fileName));
     }
 }
